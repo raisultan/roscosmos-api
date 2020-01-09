0a4c0fb18f05e14494e375f8d6a4a6cdd3a55a45
Raysultan Karimov <ki.xbozz@gmail.com>
2020-01-10 00:40:01 +0700

    docs info updated

--- app/app/views.py
+++ app/app/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("<pre>Роскосмос API!</pre>\
-        <pre>Docs: https://documenter.getpostman.com/view/2025350/RWaEzAiG</pre>\
+        <pre>Docs: https://documenter.getpostman.com/view/6866794/SWLh5Rqu</pre>\
         <pre>GitHub: https://github.com/Raysultan/roscosmos-api</pre>\
         <pre>Роскосмос: https://www.roscosmos.ru/</pre>")
